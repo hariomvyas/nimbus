157f33111cd18fe796a082ebb9e1dd93a59f0059
Hariom Vyas <hariomkvyas@gmail.com>
2020-04-26 19:20:53 +0530

    debug changed

--- nimbus/settings.py
+++ nimbus/settings.py
@@ -27,11 +27,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0ea1f527b58e3baa278d51ae98aa3dd7295978e5b9b786e1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nimbuswebserver.herokuapp.com']
 
-ENVIRONMENT = os.environ.get('ENVIRONMENT', default='development')
+ENVIRONMENT = os.environ.get('ENVIRONMENT', default='production')
 
 # production - Security from Cross Site Scripting
 if ENVIRONMENT == 'production':
